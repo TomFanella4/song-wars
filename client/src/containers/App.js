19e10b180e553fbb487d25f25bcda642ec59569c
Tom Fanella <fanella@purdue.edu>
2017-10-14 15:39:05 -0400

    Add 404 route

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import { Sidebar, Segment } from 'semantic-ui-react'
 
 import '../styles/Root.css';
@@ -7,6 +7,7 @@ import TopMenu from './TopMenu';
 import SidebarMenu from '../components/SidebarMenu';
 import Home from './Home';
 import Search from './Search';
+import NoMatch from '../components/NoMatch';
 
 const App = () => (
   <div>
@@ -17,8 +18,12 @@ const App = () => (
       <SidebarMenu />
       <Sidebar.Pusher>
         <Segment basic style={{overflowX: 'auto'}}>
-          <Route exact path='/' component={Home}/>
-          <Route path='/search' component={Search}/>
+          <Switch>
+            <Route exact path='/' component={Home}/>
+            <Route path='/home' component={Home} />
+            <Route path='/search' component={Search}/>
+            <Route component={NoMatch} />
+          </Switch>
         </Segment>
       </Sidebar.Pusher>
     </Sidebar.Pushable>
