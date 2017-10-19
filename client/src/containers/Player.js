a6ca72c07e0b2599fc28c0babe0175078fef8cb0
Tom Fanella <fanella@purdue.edu>
2017-10-19 02:18:02 -0400

    Add width scaling to Player

--- client/src/containers/Player.js
+++ client/src/containers/Player.js
@@ -1,22 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-const Player = ({ playerURI }) => (
-  <iframe
-    title='player'
-    src={
-      playerURI ?
-        `https://open.spotify.com/embed?uri=${playerURI}`
-      :
-        'https://open.spotify.com/embed?uri=spotify:track:78WVLOP9pN0G3gRLFy1rAa'
-    }
-    width={window.innerWidth}
-    height="80"
-    frameBorder="0"
-    allowTransparency="true"
-    style={{position: 'fixed', bottom: 0}}
-  />
-);
+class Player extends Component {
+  state = { width: window.innerWidth };
+
+  updateDimensions() {
+    this.setState({ width: window.innerWidth });
+  }
+
+  componentDidMount() {
+    window.addEventListener("resize", this.updateDimensions.bind(this));
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateDimensions.bind(this));
+  }
+
+  render() {
+    return (
+      <iframe
+        title='player'
+        src={
+          this.props.playerURI ?
+            `https://open.spotify.com/embed?uri=${this.props.playerURI}`
+          :
+            'https://open.spotify.com/embed?uri=spotify:track:78WVLOP9pN0G3gRLFy1rAa'
+        }
+        width={this.state.width}
+        height="80"
+        frameBorder="0"
+        allowTransparency="true"
+        style={{position: 'fixed', bottom: 0}}
+      />
+    );
+  }
+}
 
 const mapStateToProps = state => ({
   playerURI: state.playerURI
