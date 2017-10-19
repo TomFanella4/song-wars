d8ba3656865bf5508acb161fb9b7f4e4138d8c66
Tom Fanella <fanella@purdue.edu>
2017-10-19 02:53:07 -0400

    Add popularity indicator

--- client/src/containers/SearchResult.js
+++ client/src/containers/SearchResult.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Card, Button, Image } from 'semantic-ui-react';
+import { Card, Button, Image, Icon } from 'semantic-ui-react';
 import { connect } from 'react-redux';
 
 import {
@@ -35,11 +35,17 @@ class SearchResult extends Component {
             <Image src={result.album.images[2].url} floated='right' size='mini' />
           }
           <Card.Header>
-            {result.name}
+            {result.name + ' '}
+            {
+              result.popularity <= 50 &&
+              <Icon name='diamond' color='red' />
+            }
           </Card.Header>
           <Card.Meta>
-            {result.artists[0].name + ' ' + result.popularity}
+            {result.artists[0].name}
           </Card.Meta>
+        </Card.Content>
+        <Card.Content>
 
           <Button
             icon='play'
@@ -48,12 +54,16 @@ class SearchResult extends Component {
           />
 
           <Button
+            content='Recommend'
             icon='thumbs up'
+            labelPosition='left'
             color='yellow'
             disabled={this.props.recommendedSongs[result.id] ? true : false}
             loading={this.props.recommendedSongs[result.id] && this.props.recommendedSongs[result.id].loading}
             onClick={this.handleRecommendButtonClick.bind(this, result)}
           />
+          
+          {result.popularity}
         </Card.Content>
       </Card>
     )
