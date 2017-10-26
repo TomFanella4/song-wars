a5e0bf32e9b4cbaa18e83aba6d903e2f7330fde9
Ben Maxfield <bmaxfie@purdue.edu>
2017-10-26 17:02:57 -0400

    Changed how round is identified in GetVote.

--- lambda/src/main/java/com/songwars/api/brackets/current/vote/GetVoteMatchup.java
+++ lambda/src/main/java/com/songwars/api/brackets/current/vote/GetVoteMatchup.java
@@ -14,6 +14,8 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+import org.apache.http.annotation.Obsolete;
+
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
@@ -59,10 +61,11 @@ public class GetVoteMatchup implements RequestHandler<Map<String, Object>, Map<S
 		user_id = Validate.sqlstring(querystring, "user_id");
 		access_token = Validate.sqlstring(querystring, "access_token");
 		bracket_id = Validate.sqlstring(path, "bracket-id");
-		
+
+		// @Obsolete - Round is now identified through its own sql query.
 		// Get which round is supposed to be voted on today:
 		//round = Rounds.getFromMillis(System.currentTimeMillis());
-		round = Rounds.getFromEnviron();
+		//round = Rounds.getFromEnviron();
 		
 		// Database Connection:
 		Connection con = Utilities.getRemoteConnection(context);
@@ -78,9 +81,20 @@ public class GetVoteMatchup implements RequestHandler<Map<String, Object>, Map<S
 			result.close();
 			statement.close();
 			
+			// Get which round is supposed to be voted on today:
+			query = "SELECT round FROM last_week_bracket WHERE (bracket_id, round) IN ( SELECT bracket_id, MAX(round) FROM last_week_bracket WHERE bracket_id=? ) LIMIT 1";
+			PreparedStatement pstatement = con.prepareStatement(query);
+			pstatement.setString(1, bracket_id);
+			result = pstatement.executeQuery();
+			
+			if (result.next())
+				round = result.getInt("round");
+			else
+				throw new RuntimeException("[InternalServerError] - No maximum round could be identified for bracket_id: " + bracket_id);
+			
 			// Get the positions of votes that have already been cast:
 			query = "SELECT * FROM users_last_week_bracket WHERE user_id=? AND bracket_id=? AND round=?";
-			PreparedStatement pstatement = con.prepareStatement(query);
+			pstatement = con.prepareStatement(query);
 			pstatement.setString(1, user_id);
 			pstatement.setString(2, bracket_id);
 			pstatement.setInt(3, round);
