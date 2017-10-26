4a3eb2f9375a3a71feaefc15b3e860ec2851ec2a
Tom Fanella <fanella@purdue.edu>
2017-10-26 14:50:58 -0400

    Change Vote to handle data from server

--- client/src/containers/Vote.js
+++ client/src/containers/Vote.js
@@ -4,50 +4,51 @@ import { Segment, Button, Transition, Divider, Header } from 'semantic-ui-react'
 
 import VoteOption from './VoteOption';
 import { setPlayerURI } from '../actions';
+import { recordVote } from '../api'
 
 class Vote extends Component {
-  songs = {
-    tracks: [
-      {
-        s1: {
-          name: 'Mischief Managed!',
-          artist: 'John Williams',
-          album_image: 'https://i.scdn.co/image/719d1759d0045a390c8e518adc2880fdd09807d3',
-          uri: 'spotify:track:3NrAMPb3hlWCmw9kepqBmM'
-        },
-        s2: {
-          name: 'Piano Man',
-          artist: 'Billy Joel',
-          album_image: 'https://i.scdn.co/image/7e28eca3ea5124d24690d8093c76eadc069f5b09',
-          uri: 'spotify:track:3FCto7hnn1shUyZL42YgfO'
-        }
-      },
-      {
-        s1: {
-          name: 'Scarborough Fair',
-          artist: 'Adagio Music',
-          album_image: 'https://i.scdn.co/image/aecee9528ed18abed2cc184c1f617645e0364e0a',
-          uri: 'spotify:track:6ppYBr3v0ee9v1VvZWSne1'
-        },
-        s2: {
-          name: 'Feels',
-          artist: 'Calvin Harris',
-          album_image: 'https://i.scdn.co/image/1166dcd37a4afe47d013fb46065556194fee81c3',
-          uri: 'spotify:track:5bcTCxgc7xVfSaMV3RuVke'
-        }
-      }
-    ]
-  };
+  // songs = {
+  //   tracks: [
+  //     {
+  //       s1: {
+  //         name: 'Mischief Managed!',
+  //         artist: 'John Williams',
+  //         album_image: 'https://i.scdn.co/image/719d1759d0045a390c8e518adc2880fdd09807d3',
+  //         uri: 'spotify:track:3NrAMPb3hlWCmw9kepqBmM'
+  //       },
+  //       s2: {
+  //         name: 'Piano Man',
+  //         artist: 'Billy Joel',
+  //         album_image: 'https://i.scdn.co/image/7e28eca3ea5124d24690d8093c76eadc069f5b09',
+  //         uri: 'spotify:track:3FCto7hnn1shUyZL42YgfO'
+  //       }
+  //     },
+  //     {
+  //       s1: {
+  //         name: 'Scarborough Fair',
+  //         artist: 'Adagio Music',
+  //         album_image: 'https://i.scdn.co/image/aecee9528ed18abed2cc184c1f617645e0364e0a',
+  //         uri: 'spotify:track:6ppYBr3v0ee9v1VvZWSne1'
+  //       },
+  //       s2: {
+  //         name: 'Feels',
+  //         artist: 'Calvin Harris',
+  //         album_image: 'https://i.scdn.co/image/1166dcd37a4afe47d013fb46065556194fee81c3',
+  //         uri: 'spotify:track:5bcTCxgc7xVfSaMV3RuVke'
+  //       }
+  //     }
+  //   ]
+  // };
   
   state = {
     voteVisible: true,
     index: 0,
     song1: {
-      data: this.songs.tracks.length > 0 ? this.songs.tracks[0].s1 : null,
+      data: this.props.voteList.matchups.length > 0 ? this.props.voteList.matchups[0].song1 : null,
       visible: true
     },
     song2: {
-      data: this.songs.tracks.length > 0 ? this.songs.tracks[0].s2 : null,
+      data: this.props.voteList.matchups.length > 0 ? this.props.voteList.matchups[0].song2 : null,
       visible: true
     }
   };
@@ -66,20 +67,45 @@ class Vote extends Component {
     });
   }
 
+  handleVoteClick(song, index) {
+    const songName = 'song' + index;
+
+    this.setState({
+      [songName]: {
+        ...song,
+        visible: !song.visible
+      }
+    });
+
+    const vote = {
+      song_id: song.data.id,
+      bracket_id: song.data.bracket_id,
+      round: this.props.voteList.round,
+      position: song.data.position
+    };
+
+    // console.log(vote);
+
+    recordVote(vote)
+    .then(res => console.log(res))
+    .catch(err => console.error(err));
+  }
+
   onVoteHide() {
     const { index } = this.state;
+    const { voteList } = this.props;
     const newIndex = index + 1;
 
-    newIndex < this.songs.tracks.length ?
+    newIndex < voteList.matchups.length ?
       this.setState({
         voteVisible: true,
         index: newIndex,
         song1: {
-          data: this.songs.tracks[newIndex].s1,
+          data: voteList.matchups[newIndex].song1,
           visible: true
         },
         song2: {
-          data: this.songs.tracks[newIndex].s2,
+          data: voteList.matchups[newIndex].song2,
           visible: true
         }
       })
@@ -92,9 +118,11 @@ class Vote extends Component {
 
   render() {
     const { voteVisible, song1, song2, index } = this.state;
+    const { voteList } = this.props;
+    // console.log(voteList);
 
     return (
-      index < this.songs.tracks.length ?
+      index < voteList.matchups.length ?
         <Segment compact>
         
           <Transition
@@ -133,13 +161,13 @@ class Vote extends Component {
                     <Button
                       color='red'
                       content={song1.data.name}
-                      onClick={() => this.setState({ song1: {...song1, visible: !song1.visible} })}
+                      onClick={this.handleVoteClick.bind(this, song1, 1)}
                     />
                     <Button.Or />
                     <Button
                       color='green'
                       content={song2.data.name}
-                      onClick={() => this.setState({ song2: {...song2, visible: !song2.visible} })}
+                      onClick={this.handleVoteClick.bind(this, song2, 2)}
                     />
                   </Button.Group>
                 </Segment>
@@ -177,11 +205,16 @@ class Vote extends Component {
   }
 }
 
+const mapStateToProps = state => ({
+  voteList: state.voteList,
+  bracketId: state.bracketId
+});
+
 const mapDispatchToProps = dispatch => ({
   setPlayerURI: uri => dispatch(setPlayerURI(uri))
 });
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(Vote);
