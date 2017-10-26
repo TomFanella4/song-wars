79564bf92a8fa7de57a2bf2fd1d7a2233d91e5e5
Tom Fanella <fanella@purdue.edu>
2017-10-26 00:45:37 -0400

    Add getCurrentBracket endpoint

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,9 +1,11 @@
 import * as auth from './auth';
 import * as search from './search';
 import * as recommend from './recommend';
+import * as bracket from './bracket';
 import * as player from './player';
 
 export const { authServer, authSpotify, refreshAuthServer } = auth;
 export const { searchSpotify } = search;
 export const { recommendSong } = recommend;
+export const { getCurrentBracket } = bracket;
 export const { getCurrentSong } = player;
