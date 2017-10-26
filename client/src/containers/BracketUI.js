5e871c8c6b4b56700a1578822a3b920e6278e857
Tom Fanella <fanella@purdue.edu>
2017-10-26 13:30:14 -0400

    BracketUI Improvements

--- client/src/containers/BracketUI.js
+++ client/src/containers/BracketUI.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
-import { Header, Button } from 'semantic-ui-react';
+import { connect } from 'react-redux';
+import { Header, Button, Card } from 'semantic-ui-react';
 import $ from 'jquery';
-import { getCurrentBracket } from '../api';
+
+import { popularCutoff } from '../common';
+import { getCurrentBracket, getCurrentVotes } from '../api';
+import { setBracketId, setVoteList } from '../actions';
+import VoteOption from './VoteOption';
 
 window.jQuery = $;
 
@@ -9,71 +14,116 @@ require('../../node_modules/jquery-bracket/dist/jquery.bracket.min.css');
 require('../../node_modules/jquery-bracket/dist/jquery.bracket.min.js');
 
 class BracketUI extends Component {
+  state = { preview1: null, preview2: null, winner: null }
 
-  onMatchHover(...args) {
-    console.log(args);
+  onMatchHover(cell) {
+    cell && this.setState({ preview1: cell.Option1, preview2: cell.Option2 })
   }
 
   componentDidMount() {
+    const { setBracketId, setVoteList } = this.props;
+
     getCurrentBracket()
-    .then(bracket => console.log(bracket))
-    .catch(err => console.error(err))
+    .then(bracket => {
+      console.log(bracket)
 
-    var data = {
-      teams : [
-        ["Piano Man",  "Feels" ],
-        ["Batman Theme",  "Scarborough Fair" ],
-        ["Believer",  "Look What You Made Me Do" ],
-        ["I Don’t Wanna Live Forever",  "Radioactive" ]
-      ],
-      results : [
-        [[1,2, ["Piano Man",  "Feels" ]], [3,4], [5,6], [7,8]],
-        [[9,20], [7,2]],
-        [[40,3]]
-      ]
-    };
-    
-    $('.leftBracket').bracket({
-      skipConsolationRound: true,
-      teamWidth: 100,
-      matchMargin: 50,
-      onMatchHover: this.onMatchHover,
-      init: data
-    });
+      const leftBracket = {
+        teams: bracket.RightSide[0].map(cell => [cell.Option1.name, cell.Option2.name]),
+        results: bracket.RightSide.map(col => col.map(cell => [cell.Option1.votes, cell.Option2.votes, cell]))
+      };
 
-    $('.rightBracket').bracket({
-      skipConsolationRound: true,
-      teamWidth: 100,
-      matchMargin: 50,
-      onMatchHover: this.onMatchHover,
-      dir: 'rl',
-      init: data
-    });
+      const rightBracket = {
+        teams: bracket.LeftSide[0].map(cell => [cell.Option1.name, cell.Option2.name]),
+        results: bracket.LeftSide.map(col => col.map(cell => [cell.Option1.votes, cell.Option2.votes, cell]))
+      };
+
+      if (bracket.Winner) {
+        this.setState({ winner: bracket.Winner });
+      }
+
+      // var data = {
+      //   teams : [
+      //     ["Piano Man",  "Feels" ],
+      //     ["Batman Theme",  "Scarborough Fair" ],
+      //     ["Believer",  "Look What You Made Me Do" ],
+      //     ["I Don’t Wanna Live Forever",  "Radioactive" ]
+      //   ],
+      //   results : [
+      //     [[1,2], [3,4], [5,6], [7,8]],
+      //     [[9,20], [7,2]],
+      //     [[40,3]]
+      //   ]
+      // };
+      
+      $('.leftBracket').bracket({
+        skipConsolationRound: true,
+        teamWidth: 100,
+        matchMargin: 40,
+        onMatchHover: this.onMatchHover.bind(this),
+        init: leftBracket
+      });
+  
+      $('.rightBracket').bracket({
+        skipConsolationRound: true,
+        teamWidth: 100,
+        matchMargin: 40,
+        onMatchHover: this.onMatchHover.bind(this),
+        dir: 'rl',
+        init: rightBracket
+      });
+
+      setBracketId(bracket.BracketId);
+
+      getCurrentVotes()
+      .then(list => setVoteList(list))
+      .catch(err => console.error(err))
+    })
+    .catch(err => console.error(err));
   }
 
   render() {
+    const { preview1, preview2, winner } = this.state;
+
     return (
-      <div style={{ display: 'flex' }}>
-        <div>
-          <Header as='h1' content='Hidden Gems' icon='diamond' color='red' />
-          <span className="leftBracket" />
+      <div>
+        <div style={{ display: 'flex' }}>
+          <div>
+            <Header as='h1' content='Hidden Gems' icon='diamond' color='red' />
+            <span className="leftBracket" />
+          </div>
+          <span style={{ width: 175, lineHeight: 8 }}>
+            <Header content='VS' style={{ fontSize: 75 }} />
+            {
+              winner ?
+                <Header
+                  as='h1'
+                  content={'Winner:\n' + winner.name}
+                  color={winner.popularity < popularCutoff ? 'red' : 'green'}
+                />
+              :
+                <Button content='Vote' color='green' size='huge' onClick={this.props.changeView} />
+            }
+          </span>
+          <div style={{ marginLeft: 40 }}>
+            <Header as='h1' content='Popular' icon='star' color='green' />
+            <span className="rightBracket" />
+          </div>
         </div>
-        <span style={{ width: 175, lineHeight: 8 }}>
-          <Header content='VS' style={{ fontSize: 75 }} />
-          {
-            false ?
-              <Header as='h1' content={'Winner:\nScarborough Fair'} color='red' />
-            :
-              <Button content='Vote' color='green' size='huge' onClick={this.props.changeView} />
-          }
-        </span>
-        <div style={{ marginLeft: 40 }}>
-          <Header as='h1' content='Popular' icon='star' color='green' />
-          <span className="rightBracket" />
-        </div>
+        <Card.Group style={{ justifyContent: 'center' }}>
+          {preview1 && <VoteOption song={preview1} />}
+          {preview2 && <VoteOption song={preview2} />}
+        </Card.Group>
       </div>
     );
   }
 }
 
-export default BracketUI;
+const mapDispatchToProps = dispatch => ({
+  setBracketId: id => dispatch(setBracketId(id)),
+  setVoteList: list => dispatch(setVoteList(list))
+});
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(BracketUI);
