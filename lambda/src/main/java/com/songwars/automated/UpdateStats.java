80eb9aec6ea7037114d33260c08d3397dcae3490
Ben Maxfield <bmaxfie@purdue.edu>
2017-10-29 22:28:57 -0400

    Added more documentation and started first query for UpdateStats.

--- lambda/src/main/java/com/songwars/automated/UpdateStats.java
+++ lambda/src/main/java/com/songwars/automated/UpdateStats.java
@@ -1,6 +1,8 @@
 package com.songwars.automated;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -19,12 +21,29 @@ public class UpdateStats implements RequestHandler<Object, String> {
 	/**
 	 * The current stats to be displayed on the website are: 
 	 * 
-	 * # of recommendations for this week
+	 * # of recommendations for this week (inserted from migrate brackets!)
+	 * // TODO: Add this to MigrateBrackets!
+	 * id = 1, name = "recommendations", value = INT
+	 * 
 	 * # of votes counted
+	 * id = 2, name = "votes", value = INT
+	 * 
 	 * # Most nominated artists
+	 * id = 3, name = "[artist1]", value = number of songs recommended
+	 * id = 4, name = "[artist2]", value = number of songs rec...
+	 * ...
+	 * id = 12, name = "[artist10]", value = number of songs recommended
+	 * 
 	 * # Top winning songs
+	 * id = 13, name = "[songname1]", value = number of wins
+	 * id = 14, name = "[songname2]", value = number of ...
+	 * ...
+	 * id = 22, name = "[songname10]", value = number of wins
+	 * 
 	 */
 	
+
+	// TODO: Implement this with attention to multiple possible brackets.
 	
 	@Override
 	public String handleRequest(Object input, Context context) {
@@ -35,15 +54,24 @@ public class UpdateStats implements RequestHandler<Object, String> {
 		this.logger = context.getLogger();
 		// Local Variables:
 		String bracket_id = null;
-		ArrayList<HashMap<String, Object>> gems = new ArrayList<HashMap<String, Object>>();
-		ArrayList<HashMap<String, Object>> pop = new ArrayList<HashMap<String, Object>>();
-		HashMap<String, Object> song = null;
+		int total_votes = 0;
 		// Database Connection:
 		Connection con = Utilities.getRemoteConnection(context);
 		
 		try {
 			// Make Insert and Delete ONE TRANSACTION:
 			con.setAutoCommit(false);
+			
+			
+			// Get second stat: # of votes counted
+			String query = "SELECT votes FROM last_week_bracket";
+			PreparedStatement pstatement = con.prepareStatement(query);
+			ResultSet result = pstatement.executeQuery();
+			con.commit();
+			
+			while (result.next()) {
+				total_votes += result.getInt("votes");
+			}
 
 			//pstatement.close();
 			
