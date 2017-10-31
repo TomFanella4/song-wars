d573ccb7967a65793eb5238fb72fe7db3fd9ab7a
Tom Fanella <fanella@purdue.edu>
2017-10-31 01:14:01 -0400

    Add stats view

--- client/src/components/Statistics.js
+++ client/src/components/Statistics.js
@@ -1,12 +1,75 @@
 import React, { Component } from 'react';
-import { Header } from 'semantic-ui-react';
+import { Grid, Header, Loader } from 'semantic-ui-react';
+
+import { getStats } from '../api';
 
 class Statistics extends Component {
+  state = { stats: null };
+
+  componentDidMount() {
+    getStats()
+    .then(data => this.setState({ stats: data }))
+    .catch(err => console.error(err));
+  }
+
   render() {
+    const { stats } = this.state;
+    console.log(stats);
+
     return (
-      <div>
-        <Header as='h1' content='Statistics' />
-      </div>
+        stats ?
+          <div>
+            <Grid columns={3} textAlign='center' divided >
+
+              <Grid.Column>
+                <Header as='h2' content='Top Artists' />
+                <Grid columns={2} textAlign='center' >
+                  <Grid.Column>
+                    <Header as='h3' content='Artists' />
+                    {
+                      stats.top_artists.map((artist, i) => (
+                        <Header as='h4' key={i} content={artist.name} />
+                      ))
+                    }
+                  </Grid.Column>
+                  <Grid.Column>
+                    <Header as='h3' content='Votes' />
+                    {
+                      stats.top_artists.map((artist, i) => (
+                        <Header as='h4' key={i} content={artist.count} />
+                      ))
+                    }
+                  </Grid.Column>
+                </Grid>
+              </Grid.Column>
+
+              <Grid.Column>
+                <Header as='h2' content='Totals' textAlign='center' />
+                <Grid columns={2} textAlign='center' >
+                  <Grid.Column>
+                    <Header as='h2' content='Votes' />
+                    <Header as='h2' content='Recommendations' />
+                  </Grid.Column>
+                  <Grid.Column>
+                    <Header as='h2' content={stats.votes} />
+                    <Header as='h2' content={stats.recommendations} />
+                  </Grid.Column>
+                </Grid>
+              </Grid.Column>
+
+              <Grid.Column>
+                <Header as='h2' content='Top Songs' />
+                {
+                  // stats.top_songs.map((song, i) => (
+                  //   <Header as='h3' key={i} content={song.name + ' ' + song.count} />
+                  // ))
+                }
+              </Grid.Column>
+
+            </Grid>
+          </div>
+        :
+          <Loader active={true} />
     );
   }
 }
