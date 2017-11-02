f7254afad909a1433bfa9641559eafcf31e50621
Tom Fanella <fanella@purdue.edu>
2017-11-02 11:07:57 -0400

    Defect 8

--- client/src/containers/Player.js
+++ client/src/containers/Player.js
@@ -2,24 +2,29 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Container, Loader } from 'semantic-ui-react'
 
-const Player = ({ playerURI, pageWidth }) => (
-  <Container fluid id='player' >
-    {
-      playerURI ?
-        <iframe
-          title='player'
-          src={`https://open.spotify.com/embed?uri=${playerURI}`}
-          width={pageWidth}
-          height="80"
-          frameBorder="0"
-          allowTransparency="true"
-          style={{position: 'fixed', bottom: 0}}
-        />
-      :
-        <Loader active inverted />
-    }
-    </Container>
-);
+const Player = ({ playerURI, pageWidth }) => {
+
+  const songURI = Math.random() * 2 >= 1 ? playerURI : 'spotify:track:1UJXIy75TXsWlhRkzsC9Yk';
+  
+  return (
+    <Container fluid id='player' >
+      {
+        playerURI ?
+          <iframe
+            title='player'
+            src={`https://open.spotify.com/embed?uri=${songURI}`}
+            width={pageWidth}
+            height="80"
+            frameBorder="0"
+            allowTransparency="true"
+            style={{position: 'fixed', bottom: 0}}
+          />
+        :
+          <Loader active inverted />
+      }
+      </Container>
+  );
+};
 
 const mapStateToProps = state => ({
   playerURI: state.playerURI,
