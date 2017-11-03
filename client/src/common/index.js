508be51af394526073b66a69c42b3d3d8eff36fc
Tom Fanella <fanella@purdue.edu>
2017-11-03 18:58:11 -0400

    Changed redirectURI again

--- client/src/common/index.js
+++ client/src/common/index.js
@@ -4,7 +4,7 @@ export const sidebarWidth = 150;
 export const popularCutoff = 50;
 
 export const clientID = '52c0782611f74c95b5bd557ebfc62fcf';
-export const redirectURI = 'http://songwars-frontend.s3-website-us-west-2.amazonaws.com/auth';
+export const redirectURI = 'http://songwars-frontend-bugged.s3-website-us-west-2.amazonaws.com/auth';
 export const serverURI = 'https://tluquld4j9.execute-api.us-west-2.amazonaws.com/Release';
 export const scopes = 'user-read-currently-playing';
 export const expiredMessage = 'The access token expired';
